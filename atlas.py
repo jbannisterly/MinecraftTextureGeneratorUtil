6c5c237a17a1ababe7e3cacc6e5c36e198c86d55
jbannisterly <jbannisterly@gmail.com>
2026-07-10 23:33:24 +0100

    Added wraparound after filling a column

--- atlas.py
+++ atlas.py
@@ -6,18 +6,14 @@ class Atlas:
     def __init__(self, atlas_size):
         self.atlas = np.zeros(atlas_size)
         self.offset = [0, 0]
+        self.max_width = 0
 
     def addCube(self, source_image, size, offset):
         cube = Cube()
         cube.stretch(source_image, size, offset)
+        self.max_width = max(self.max_width, cube.x * 2 + cube.z * 2)
+        if self.offset[1] + cube.y + cube.z > self.atlas.shape[1]:
+            self.offset = [self.max_width + self.offset[0], 0]
+            self.max_width = 0
         cube.writeimage(self.atlas, self.offset)
         self.offset[1] += cube.z + cube.y
-
-example = cv2.imread("res/example.png")
-
-# example values
-atlas = Atlas((64, 64, 3))
-atlas.addCube(example, [4, 8, 8], [0, 0])
-atlas.addCube(example, [8, 2, 4], [4, 4])
-
-cv2.imwrite("res/example_copy.png", atlas.atlas)
