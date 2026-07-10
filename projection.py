c7e61bd489068d27e36ff23d3bc9946df2bd213a
jbannisterly <jbannisterly@gmail.com>
2026-07-10 23:33:37 +0100

    Sphere mapping example

--- projection.py
+++ projection.py
@@ -1,13 +1,21 @@
 import cv2
 import numpy as np
-from cube import Cube
+from atlas import Atlas
 
 example = cv2.imread("res/example.png")
 
-# example values
-atlas = np.zeros((32, 32, 3))
-cube = Cube()
-cube.stretch(example, [4, 8, 6], [2, 4])
-cube.writeimage(atlas, [3,3])
+# create the cubes for a sphere
+cubes = [
+    [16, 6, 6],
+    [14, 10, 10],
+    [6, 6, 16],
+    [10, 10, 14],
+    [12, 12, 12],
+    [10, 14, 10],
+    [6, 16, 6]
+]
+atlas = Atlas((128, 128, 3))
+for cube in cubes:
+    atlas.addCube(example, cube, [16 - cube[0], 16 - cube[1]])
 
-cv2.imwrite("res/example_copy.png", atlas)
+cv2.imwrite("res/example_copy.png", atlas.atlas)
